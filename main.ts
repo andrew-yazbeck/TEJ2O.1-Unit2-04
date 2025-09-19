7279d807ee97f736386510298cefc8be3a4ef651
andrew-yazbeck <andrew.yazbeck@stu.ocsb.ca>
2025-09-19 13:09:05 -0400

    shows the temperature

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Andrew
+ * Created on: Sep 2025
+ * This program finds the temperature
 */
 
-basic.showString('Hello, World!')
+basic.forever(function () {
+    let tempC = input.temperature();
+    basic.showNumber(tempC);
+    basic.pause(1000);
+})
